4db28667fb02b8158f66598e95de6891cf2cf322
Gopal-devhub <sappa.gopal01@gmail.com>
2025-12-15 16:38:22 +0530

    Initial commit: React project with MyComponent

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import Mycomponent from "./Mycomponent" 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <p>
+          Edit <code>src/App.js</code>
+        </p>
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          
+        >
+        </a>
+        <p>
+          <Mycomponent/>
+      </p>
+      </header>
+      
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/Mycomponent.js
@@ -0,0 +1,9 @@
+function component()
+{
+    return (
+        <div>
+            <h1>HEllo react js </h1>
+        </div>
+    )
+}
+export default component;
